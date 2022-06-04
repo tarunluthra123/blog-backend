d905a489fbf379bcb1742d3b779ded93467c1e22
Tarun Luthra <tarunluthra987@gmail.com>
2022-06-04 20:57:11 +0530

    Create user route

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from api.views import PingPongView
+from api.views import PingPongView, CreateUserView
 
 urlpatterns = [
     path("ping/", PingPongView.as_view()),
+    path("users/", CreateUserView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView
+
+from api.models import User
+from api.serializers import UserSerializer
 
 
 class PingPongView(APIView):
     def get(self, request):
         return Response({"message": "pingpong"})
+
+
+class CreateUserView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
