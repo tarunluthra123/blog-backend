7c703ba4dc3540387759ecdcbb8fb79225275329
Tarun Luthra <tarunluthra987@gmail.com>
2022-09-24 15:36:23 +0530

    Comment model

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from api.models import User, Article, ArticleTag
+from api.models import User, Article, ArticleTag, Comment
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Article)
 admin.site.register(ArticleTag)
+admin.site.register(Comment)
